ead4b6afc3930d49d3c0d788dece1474f8351682
acedunn <acedunn@gmail.com>
2021-09-04 13:48:35 -0700

    Initialize ryan/eth-provider

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+    },
+  },
+  testTimeout: 40000,
+  coveragePathIgnorePatterns : [
+    '/node_modules/',
+    '/src/tests/',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/jestSetup.ts'],
+};
